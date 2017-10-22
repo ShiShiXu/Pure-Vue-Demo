98e6b616605eb991e021d230644d86a7e743dba6
K.S <1093806808@qq.com>
2017-10-22 23:19:40 +0800

    缩进代码

--- js/component.js
+++ js/component.js
@@ -12,14 +12,14 @@ Vue.component('modal', {
         }
     },
     template: '<div class="flex cross-center modal">'+
-    '<div class="modal-box">'+
-    '<div class="flex main-center modal-title">{{ modalTitle }}</div>'+
-    '<div class="modal-body">'+
-    '<div> {{ modalMsg }} </div>'+
-    '</div>'+
-    '<div class="modal-footer">'+
-    '<button id="app1-modal-btn" type="button" class="modal-button active" @click="modalButtonClick">确认</button>'+
-    '</div>'+
-    '</div>'+
-    '</div>'
+                    '<div class="modal-box">'+
+                        '<div class="flex main-center modal-title">{{ modalTitle }}</div>'+
+                        '<div class="modal-body">'+
+                            '<div> {{ modalMsg }} </div>'+
+                        '</div>'+
+                        '<div class="modal-footer">'+
+                            '<button id="app1-modal-btn" type="button" class="modal-button active" @click="modalButtonClick">确认</button>'+
+                        '</div>'+
+                    '</div>'+
+               '</div>'
 });
